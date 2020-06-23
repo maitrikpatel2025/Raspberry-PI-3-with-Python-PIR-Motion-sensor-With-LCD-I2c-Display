ce9e81fab1aee89883e819c45a42dd508ddf50aa
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2020-06-23 07:54:49 -0400

    Update Raspberry PI 3 with Python PIR Motion sensor With LCD(I2c) Display.py

--- Raspberry PI 3 with Python PIR Motion sensor With LCD(I2c) Display.py
+++ Raspberry PI 3 with Python PIR Motion sensor With LCD(I2c) Display.py
@@ -11,10 +11,7 @@ import signal
   import RPi.GPIO as GPIO 
   from Adafruit_CharLCD import Adafruit_CharLCD 
   
-lcd = Adafruit_CharLCD (rs = 26, en = 19, d4 = 13, d5 = 6, d6 = 5, 
-d7 =
-			   11, cols = 16, lines =
-			   2) 
+lcd = Adafruit_CharLCD (rs = 26, en = 19, d4 = 13, d5 = 6, d6 = 5, d7 =11, cols = 16, lines =2) 
  
 lcd.clear () 
  
